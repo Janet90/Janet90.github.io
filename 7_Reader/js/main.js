d35813eeda57936ce3028688f2a2aa3c9cb7a4d6
Janet90 <31942825+Janet90@users.noreply.github.com>
2018-06-08 17:47:30 +1000

    commit
    new

--- 7_Reader/js/main.js
+++ 7_Reader/js/main.js
@@ -5,17 +5,28 @@ $(document).ready(function() {
 
 // Add jQuery here
 
-
-
-
-
-
 $("#box").click(function() {
   $('html, body').animate({
       scrollTop: $("#box").offset().top
   }, 2000);
 });
 
+
+$('.e-hearts').mouseenter(function() {
+    $('.p-hearts').addClass('p-show');
+  });
+
+$('.e-hearts').mouseleave(function() {
+    $('.p-hearts').removeClass('p-show');
+  });
+
+$('.e-flag').mouseenter(function() {
+    $('.p-flag').addClass('p-show');
+  });
+
+$('.e-flag').mouseleave(function() {
+    $('.p-flag').removeClass('p-show');
+  });
 
 
 
